30014cc9360fa2e139f9399544bd5dffac92ac13
Furrium Network <furrium@hotmail.com>
2026-08-09 12:30:08 +0800

    Update config.js
    将BASEURL改为了相对路径

--- config.js
+++ config.js
@@ -6,11 +6,8 @@
 const CONFIG = {
   // ===== 必填 =====
   
-  // 后端 API 地址
-  // - Docker 部署：http://backend:3000/api
-  // - 本地开发：http://localhost:3000/api
-  // - 生产环境：https://你的域名/api
-  API_BASE_URL: 'http://localhost:3000/api',
+
+  API_BASE_URL: '/api',
 
   // ===== 可选 =====
 
